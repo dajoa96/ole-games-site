52f920c93c5631d192c43992d5551cb44a8df50d
David Ortega <david96.des@gmail.com>
2022-11-18 16:42:58 -0400

    Se agregó el link de video para Ole Cats

--- src/app/modules/games/games.component.ts
+++ src/app/modules/games/games.component.ts
@@ -13,6 +13,8 @@ export class GamesComponent implements OnInit {
       image: '/assets/images/games/ole-cats/ole-cats-image.jpeg',
       description: 'Partidas intensas 1 vs 1 Acaba con tu rival para conseguir el premio. Cada gato tiene 7 vidas, el primero que se quede sin ellas, pierde. Usa los power ups para obtener habilidades.',
       link: '#',
+      isVideo: true,
+      videoLink: 'https://www.youtube.com/embed/Bd71DwGl7hs'
 
     },
     {
@@ -20,7 +22,7 @@ export class GamesComponent implements OnInit {
       image: '/assets/images/games/ole-racing/ole-racing-image.png',
       description: 'Modo clásico con múltiples pistas para competir contra otro amigo. Usa los power ups para obtener habilidades aleatorias.',
       link: '#',
-
+      isVideo: false
     }
   ];
 
@@ -31,5 +33,4 @@ export class GamesComponent implements OnInit {
   ngOnInit(): void {
     this.titleService.setTitle('Ole Games - Juegos');
   }
-
 }
