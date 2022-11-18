eff1714c3d2a88aa2c1b21c5a4741a1b2ebadf5e
David Ortega <david96.des@gmail.com>
2022-11-18 13:21:10 -0400

    Lista estática de los NFT actualmente en el sitio de Ole Games

---
+++ src/app/mocks/nfts.mock.ts
@@ -0,0 +1,112 @@
+import { StandardNft } from "../models/standard-nft.model";
+
+export const NFTSMock: any[] = [
+  {
+    title: "Jessica La Fosfoyesica",
+    image: "/assets/images/nft/jessica.png",
+    external_url: "#",
+    price: 0.15,
+    currency: 'BNB',
+    units: 10000,
+    bettingRange: '(De 1 a 50.000 USDT)',
+    earningsPerGame: 95,
+    earningsPerTournament: 95,
+    featured: true
+  },
+  {
+    title: "Común",
+    image: "/assets/images/nft/jessica-2d.png",
+    external_url: "#",
+    price: 0.025,
+    currency: 'BNB',
+    units: 10000,
+    bettingRange: '1 - 5 - 10 USD',
+    earningsPerGame: 85,
+    earningsPerTournament: 10,
+    featured: false
+  },
+  {
+    title: "Poco Común",
+    image: "/assets/images/nft/jessica-pistola.png",
+    external_url: "#",
+    price: 0.075,
+    currency: 'BNB',
+    units: 10000,
+    bettingRange: '15 - 20 - 30 USD',
+    earningsPerGame: 86.25,
+    earningsPerTournament: 20,
+    featured: false
+  },
+  {
+    title: "Super",
+    image: "/assets/images/nft/jessica-vr.png",
+    external_url: "#",
+    price: 0.125,
+    currency: 'BNB',
+    units: 10000,
+    bettingRange: '35 - 40 - 50 USD',
+    earningsPerGame: 87.5,
+    earningsPerTournament: 30,
+    featured: false
+  },
+  {
+    title: "Raro",
+    image: "/assets/images/nft/jessica-arcade.png",
+    external_url: "#",
+    price: 0.25,
+    currency: 'BNB',
+    units: 10000,
+    bettingRange: '60 - 80 - 100 USD',
+    earningsPerGame: 88.75,
+    earningsPerTournament: 40,
+    featured: false
+  },
+  {
+    title: "Épico",
+    image: "/assets/images/nft/jessica-buggie.png",
+    external_url: "#",
+    price: 0.75,
+    currency: 'BNB',
+    units: 10000,
+    bettingRange: '300 - 600 - 1.000 USD',
+    earningsPerGame: 90,
+    earningsPerTournament: 50,
+    featured: false
+  },
+  {
+    title: "Legendario",
+    image: "/assets/images/nft/jessica-tanque.png",
+    external_url: "#",
+    price: 1.2,
+    currency: 'BNB',
+    units: 10000,
+    bettingRange: '2.000 - 5.000 - 10.000 USD',
+    earningsPerGame: 91.25,
+    earningsPerTournament: 60,
+    featured: false
+  },
+  {
+    title: "Bárbaro",
+    image: "/assets/images/nft/jessica-liberator.png",
+    external_url: "#",
+    price: 2.5,
+    currency: 'BNB',
+    units: 10000,
+    bettingRange: '15.000 - 20.000 - 30.000 USD',
+    earningsPerGame: 92.5,
+    earningsPerTournament: 75,
+    featured: false
+  },
+  {
+    title: "Destructor",
+    image: "/assets/images/nft/jessica-battlecruiser.png",
+    external_url: "#",
+    price: 12,
+    currency: 'BNB',
+    units: 10000,
+    bettingRange: '40.000 - 45.000 - 50.000 USD',
+    earningsPerGame: 95,
+    earningsPerTournament: 95,
+    featured: false
+  }
+];
