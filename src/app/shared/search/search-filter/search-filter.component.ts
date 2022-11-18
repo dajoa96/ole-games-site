6a12394100980ddbf119a958e3ce31336bf08c71
David Ortega <david96.des@gmail.com>
2022-11-18 13:23:37 -0400

    Componente de barra de búsqueda, funciona con data estática por ahora

--- src/app/shared/search/search-filter/search-filter.component.ts
+++ src/app/shared/search/search-filter/search-filter.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnDestroy, OnInit, Output } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { debounceTime, distinctUntilChanged, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-search-filter',
   templateUrl: './search-filter.component.html',
   styleUrls: ['./search-filter.component.scss']
 })
-export class SearchFilterComponent implements OnInit {
+export class SearchFilterComponent implements OnInit, OnDestroy {
+  @Output('onSearch') onSearch: EventEmitter<any> = new EventEmitter;
+  searchControl: FormControl = new FormControl('');
+  searchFilter?: any;
+  searchSub$?: Subscription;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.searchSub$ = this.searchControl.valueChanges.pipe(
+      debounceTime(400),
+      distinctUntilChanged()
+    ).subscribe(s => this.onNewSearch(s));
+  }
+
+  ngOnDestroy(): void {
+    this.searchSub$?.unsubscribe();
+  }
+
+  onNewSearch(value: string) {
+    this.onSearch.emit(value);
   }
 
 }
